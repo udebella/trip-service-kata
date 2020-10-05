c97cbbae346a22b5eacb81e8e9e31cf91cca7543
Ubu <ubald2b@gmail.com>
2020-10-05 23:43:18 +0200

    Factorized test initializations

--- javascript/test/TripServiceSpec.js
+++ javascript/test/TripServiceSpec.js
@@ -9,15 +9,21 @@ const {stub} = sinon
 const {expect} = chai
 
 describe('TripService', () => {
+    let tripService, stubs
+    beforeEach(() => {
+        stubs = {
+            getLoggedUser: stub(),
+            findTripsByUser: stub(),
+        }
+        tripService = tripsFinder(stubs)
+    })
 
     it('should fail when given a null parameter', () => {
-        const tripService = tripsFinder({getLoggedUser: stub()})
-
         expect(() => tripService.getTripsByUser(null)).to.throw('User not logged in.')
     })
 
     it('should return empty list when connected user is not a friend', () => {
-        const tripService = tripsFinder({getLoggedUser: stub().returns(new User())})
+        stubs.getLoggedUser.returns(new User())
 
         const tripList = tripService.getTripsByUser(new User())
 
@@ -26,10 +32,8 @@ describe('TripService', () => {
 
     it('should return trip list when connected user is a friend', () => {
         const connectedUser = new User();
-        const tripService = tripsFinder({
-            getLoggedUser: stub().returns(connectedUser),
-            findTripsByUser: stub().returns(['Paris'])
-        })
+        stubs.getLoggedUser.returns(connectedUser)
+        stubs.findTripsByUser.returns(['Paris'])
 
         const tripList = tripService.getTripsByUser(new User([connectedUser]))
 
