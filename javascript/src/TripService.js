72ee01a55cae04a9178cb026e7c756a93f4e5706
Ubu <ubald2b@gmail.com>
2020-10-06 00:03:38 +0200

    Extract function to retrieve connected user

--- javascript/src/TripService.js
+++ javascript/src/TripService.js
@@ -4,11 +4,16 @@ import UserSession from './UserSession.js'
 import TripDAO from './TripDAO.js'
 
 export const tripsFinder = ({getLoggedUser = UserSession.getLoggedUser, findTripsByUser = TripDAO.findTripsByUser} = {}) => {
-    function getTripsByUser(user) {
+    function failIfNotConnected() {
         const loggedUser = getLoggedUser()
         if (!loggedUser) {
             throw new Error('User not logged in.')
         }
+        return loggedUser;
+    }
+
+    function getTripsByUser(user) {
+        const loggedUser = failIfNotConnected();
 
         return user
             .getFriends()
