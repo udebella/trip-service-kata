8512ff4d94d93f329bfbbecb09ee66a53f06262f
Ubu <ubald2b@gmail.com>
2020-10-06 00:40:38 +0200

    Extract ifConnected function outside TripService

--- javascript/src/TripService.js
+++ javascript/src/TripService.js
@@ -3,24 +3,22 @@
 import UserSession from './UserSession.js'
 import TripDAO from './TripDAO.js'
 
-export function TripService({getLoggedUser = UserSession.getLoggedUser, findTripsByUser = TripDAO.findTripsByUser} = {}) {
-    const ifConnected = () => ({
-        then: callBack => {
-            const loggedUser = getLoggedUser()
-            if (!loggedUser) {
-                throw new Error('User not logged in.')
-            }
-            return callBack(loggedUser)
+const ifConnected = (getLoggedUser) => ({
+    then: callBack => {
+        const loggedUser = getLoggedUser()
+        if (!loggedUser) {
+            throw new Error('User not logged in.')
         }
-    })
+        return callBack(loggedUser)
+    }
+})
 
-    const getTripsByUser = user => ifConnected()
-        .then(loggedUser => user
-            .getFriends()
-            .filter(friend => friend === loggedUser)
-            .flatMap(_ => findTripsByUser(user)))
-
+export function TripService({getLoggedUser = UserSession.getLoggedUser, findTripsByUser = TripDAO.findTripsByUser} = {}) {
     return {
-        getTripsByUser
+        getTripsByUser: user => ifConnected(getLoggedUser)
+            .then(loggedUser => user
+                .getFriends()
+                .filter(friend => friend === loggedUser)
+                .flatMap(_ => findTripsByUser(user)))
     }
 }
