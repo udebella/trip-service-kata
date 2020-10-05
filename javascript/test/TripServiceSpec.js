754527bbd69cd84936130370a0c32d76bb37484c
Ubu <ubald2b@gmail.com>
2020-10-05 22:51:50 +0200

    Add test about value returned when connected user is not a friend

--- javascript/test/TripServiceSpec.js
+++ javascript/test/TripServiceSpec.js
@@ -3,6 +3,7 @@
 import chai from 'chai'
 import TripService from '../src/TripService.js'
 import sinon from 'sinon'
+import User from '../src/User.js'
 const {stub} = sinon
 const {expect} = chai
 
@@ -13,6 +14,15 @@ describe('TripService', () => {
         const tripService = new TripService(userSession)
 
         expect(() => tripService.getTripsByUser(null)).to.throw('User not logged in.')
+    })
+
+    it('should return empty list when connected user is not a friend', () => {
+        const userSession = {getLoggedUser: stub().returns(new User())}
+        const tripService = new TripService(userSession)
+
+        const tripList = tripService.getTripsByUser(new User())
+
+        expect(tripList).to.deep.equal([])
     })
 
 })
