c147a8810ff268ed2a258177a5d170d29e602c44
Ubu <ubald2b@gmail.com>
2020-10-05 23:19:14 +0200

    Refactoring TripService to use factory function to remove use of this

--- javascript/src/TripService.js
+++ javascript/src/TripService.js
@@ -3,7 +3,7 @@
 import UserSession from './UserSession.js'
 import TripDAO from './TripDAO.js'
 
-class TripService {
+class Service {
     constructor(userSession = UserSession, tripDao = TripDAO) {
         this.userSession = userSession;
         this.tripDao = tripDao;
@@ -22,4 +22,22 @@ class TripService {
     }
 }
 
-export default TripService
+export const tripsFinder = ({userSession = UserSession, tripDao = TripDAO} = {}) => {
+    function getTripsByUser(user) {
+        const loggedUser = userSession.getLoggedUser()
+        if (!loggedUser) {
+            throw new Error('User not logged in.')
+        }
+
+        return user
+            .getFriends()
+            .filter(friend => friend === loggedUser)
+            .flatMap(_ => tripDao.findTripsByUser(user))
+    }
+
+    return {
+        getTripsByUser
+    }
+}
+
+export default Service
